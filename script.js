f26f8df70e85b0e9795ec8655927867567e4cb9f
Anthony Osiol <funnynoun@gmail.com>
2020-03-06 21:55:25 -0500

    pseudo code, questions, button functions

---
+++ script.js
@@ -0,0 +1,99 @@
+// var startbtn = document.querySelector("start");
+// var timeDisplay = document.querySelector("timeDisplay");
+// var totalScores = document.querySelector("totalScores");
+
+
+
+
+// // press start button to start alerts with questions and timer
+// startbtn = addEventListener("click", function() {
+
+
+// });
+
+
+
+
+
+
+
+// // button to view stored high scores
+// totalScores = addEventListener("click", function() {
+
+
+
+// });
+
+
+
+
+
+
+// // timer, starts at 75 sec, counts down and skips 10 sec with each question answered incorrectly
+
+
+
+
+
+
+
+
+// // questions start once "start" is clicked. Brought up in random, one correct answer, three incorrect.
+// var array = [{
+//     questionOne: "What does CSS stand for?", 
+//         optionOne: "Cats Standing Still",
+//         optionTwo: "Computer Sending Signals",
+//         optionThree: "Cascading Simple Sheets",
+//         correctAnswer: "Cascading Style Sheets",
+       
+
+// //     questionTwo: "What is a tag in HTML?", 
+// //         optionOne: "A function",
+// //         optionTwo:  "Identifies the action of HTML",
+// //         optionThree: "Gives a name to the line of code",
+// //         correctAnswer: "Defines how browser will format and display",
+    
+
+// //     questionThree: "JavaScript commonly interacts with what programming languages?", 
+// //         optionOne: "French and Russian", 
+// //         optionTwo: "C++ and Java",
+// //         optionThree: "jQuery and Java",
+// //         correctAnswer: "HTML and CSS",
+    
+    
+// //     questionFour: "What is the syntax for writing a function?", 
+// //         optionOne: "var = function",
+// //         optionTwo: "name function(code to execute){parameter1, parameter2}",
+// //         optionThree: "function(parameter1, parameter2) name",
+// //         correctAnswer: "function name(parameter1, parameter2){code to execute}",
+    
+
+// //     questionfive: "JavaScript has been described as...",
+// //         optionOne: "light or dark roast.",
+// //         optionTwo: "giving design to HTML.",
+// //         optionThree: "keeping the computer from crashing.",
+// //         correctAnswer: "code that animates a website.",
+// // ]
+
+// };
+// When correctAnswer is clicked
+document.getElementById("answerB").addEventListener("click", function() {
+    // alert("yay")
+
+    function displayAnswer() {
+        document.getElementById("display").innerHTML = answer();
+    }
+});
+
+// document.getElementById("myBtn").addEventListener("click", displayDate);
+
+// function displayDate() {
+//   document.getElementById("demo").innerHTML = Date();
+// }
+
+// When option
+
+
+
+
+// end of game stops timer, adds score to list or scores
