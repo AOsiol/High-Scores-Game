e119aad978c8afd6349336159e32cf806116cdf1
Anthony Osiol <funnynoun@gmail.com>
2020-03-08 10:09:57 -0400

    I plan to add to the code to get more working

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # High-Scores-Game
+Game Outline
+
+This is a game designed to interact with the player with a start button, timer, questions to answer by clicking buttons, and a button to show high scores. 
+
+This project presented a challenge to me. I created pseudo-code for planning the interactive elements. After writting the code for the different elements, trying a few different approaches without success, I would delete portions and start over. I inserted a few things that Justin Slacked out, but that was not necessarily how I think about the approach, so I backed off and am trying different options.
--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // var startbtn = document.querySelector("start");
 // var timeDisplay = document.querySelector("timeDisplay");
 // var totalScores = document.querySelector("totalScores");
-
+var currentQuestion = 0;
+var timer = getElementById("timer");
+var time = timer.text();
 
 
 
 // // press start button to start alerts with questions and timer
-// startbtn = addEventListener("click", function() {
-
-
-// });
-
-
-
+document.getElementById("startBtn").addEventListener("click", function () {
+    appendQuestion(questions[currentQuestion]);
+    // show timer
+    startTheTimer();
+});
 
 
 
@@ -29,68 +29,86 @@
 
 
 
-// // timer, starts at 75 sec, counts down and skips 10 sec with each question answered incorrectly
+// // timer, starts at 60 sec, counts down and skips 10 sec with each question answered incorrectly
 
 
+var startTheTimer = function() {
+    timer.text(time);
 
+    var counter = setInterval(function() {
+
+        time = time - 1;
+        timer.text(time);
+    })
+    // }, 1000);
+
+};
 
 
 
 
 
 // // questions start once "start" is clicked. Brought up in random, one correct answer, three incorrect.
-// var array = [{
-//     questionOne: "What does CSS stand for?", 
-//         optionOne: "Cats Standing Still",
-//         optionTwo: "Computer Sending Signals",
-//         optionThree: "Cascading Simple Sheets",
-//         correctAnswer: "Cascading Style Sheets",
-       
+var questions = [
+    {
+        question: "What does CSS stand for?",
+        options: {
+            "Cats Standing Still",
+            "Computer Sending Signals",
+            "Cascading Simple Sheets"
+        },
+        correctAnswer: "Cascading Style Sheets"
+    },
 
-// //     questionTwo: "What is a tag in HTML?", 
-// //         optionOne: "A function",
-// //         optionTwo:  "Identifies the action of HTML",
-// //         optionThree: "Gives a name to the line of code",
-// //         correctAnswer: "Defines how browser will format and display",
-    
+    {
+        question: "What is a tag in HTML?",
+        options: {
+            "A function",
+            "Identifies the action of HTML",
+            "Gives a name to the line of code"
+        },
+        correctAnswer: "Defines how browser will format and display"
+    },
 
-// //     questionThree: "JavaScript commonly interacts with what programming languages?", 
-// //         optionOne: "French and Russian", 
-// //         optionTwo: "C++ and Java",
-// //         optionThree: "jQuery and Java",
-// //         correctAnswer: "HTML and CSS",
-    
-    
-// //     questionFour: "What is the syntax for writing a function?", 
-// //         optionOne: "var = function",
-// //         optionTwo: "name function(code to execute){parameter1, parameter2}",
-// //         optionThree: "function(parameter1, parameter2) name",
-// //         correctAnswer: "function name(parameter1, parameter2){code to execute}",
-    
+    {
+        question: "JavaScript commonly interacts with what programming languages?",
+        options: {
+            "French and Russian",
+            "C++ and Java",
+            "jQuery and Java"
+        },
+        correctAnswer: "HTML and CSS"
+    },
 
-// //     questionfive: "JavaScript has been described as...",
-// //         optionOne: "light or dark roast.",
-// //         optionTwo: "giving design to HTML.",
-// //         optionThree: "keeping the computer from crashing.",
-// //         correctAnswer: "code that animates a website.",
-// // ]
+    {
+        question: "What is the syntax for writing a function?",
+        options: {
+             "var = function",
+             "name function(code to execute){parameter1, parameter2}",
+             "function(parameter1, parameter2) name"
+        },
+        correctAnswer: "function name(parameter1, parameter2){code to execute}"
+    },
 
-// };
+    {
+        question: "JavaScript has been described as...",
+        option: {
+            "light or dark roast.",
+            "giving design to HTML.",
+            "keeping the computer from crashing."
+        },
+        correctAnswer: "code that animates a website."
+    },
+]
+
+var appendQuestion = function(anyquiz) {
+    rows.first().empty();
+    rows.last().empty();
+}
 // When correctAnswer is clicked
-document.getElementById("answerB").addEventListener("click", function() {
-    // alert("yay")
 
-    function displayAnswer() {
-        document.getElementById("display").innerHTML = answer();
-    }
-});
 
-// document.getElementById("myBtn").addEventListener("click", displayDate);
-
-// function displayDate() {
-//   document.getElementById("demo").innerHTML = Date();
-// }
-
+    
 // When option
 
 
